20a568113933979c4412075f1ee9e9490c55a060
fatucode <wannjalloh@gmail.com>
2026-04-03 16:56:07 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<!-- includes/footer.php -->
+<footer>
+    <div class="footersimple">
+        <p>📍 12 rue du Smoothie, Sunsippy | mail: hello@sunsip.fr</p>
+        <p class="instapote">Suivez-nous sur Insta : <strong>@SunSip_Fresh</strong></p>
+        <hr class="minibar">
+        <p>SunSip &copy; 2026 - Fait avec amour 🍓</p>
+    </div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<header class="headersun">
+    <div class="titlegroup">
+        <h1 class="titlepink">SunSip</h1>
+        <p class="slogan">"Frais et délicieux : Le smoothie qui défie le soleil !"</p>
+    </div>
+    <img src="../logo.jpeg" alt="Logo SunSip" class="removebg">
+</header>
+
+<nav class="navsimple">
+    <a href="../views/accueil.php">Accueil</a>
+    <a href="../views/presentation.php">Carte</a>
+    <a href="../views/inscription.php">S'inscrire</a>
+    <a href="../views/connexion.php">Connexion</a>
+    <a href="../views/profil.php">Mon profil</a>
+</nav>
