e8fa2c07549fb7bcbd241605afe6b031d9b732cb
Ramona15541 <ramona.github@gmail.com>
2026-05-24 21:07:22 +0200

    Update validation.js

--- js/validation.js
+++ js/validation.js
@@ -1,31 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const themeLink = document.querySelector("link[href*='style.css']");
-    const themeBtn = document.getElementById("theme-toggle");
-    let cookies = document.cookie.split('; ');
-    let currentTheme = "../style.css"; 
-
-    for (let c of cookies) {
-        let [name, value] = c.split('=');
-        if (name === 'theme') {
-            currentTheme = value;
-        }
-    }
-    if (themeLink && (currentTheme.includes("style.css") || currentTheme.includes("dark_style.css"))) {
-        themeLink.setAttribute("href", currentTheme);
-    }
-
-    if (themeBtn && themeLink) {
-        themeBtn.addEventListener("click", function() {
-            let activeTheme = themeLink.getAttribute("href");
-            let newTheme = "../style.css";
-            if (activeTheme.includes("dark_style.css")) {
-                newTheme = "../style.css";
-            } else {
-                newTheme = "../dark_style.css";
+    const togglePassword = document.getElementById("toggle-password");
+    const passwordInput = document.getElementById("password");
+    
+    if (togglePassword && passwordInput) {
+        
+        togglePassword.addEventListener("click", function() {
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text"; 
+                togglePassword.textContent = "🙈";
+            } 
+            else {
+                passwordInput.type = "password"; 
+                togglePassword.textContent = "👁️"; 
             }
-            themeLink.setAttribute("href", newTheme);
-
-            document.cookie = `theme=${newTheme}; max-age=${30 * 24 * 60 * 60}; path=/`;
         });
     }
+
 });
