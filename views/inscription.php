025a06627456c99fd64d1f1a748272024d7fc542
Ramona15541 <ramona.github@gmail.com>
2026-05-31 23:17:17 +0200

    Update inscription.php

--- views/inscription.php
+++ views/inscription.php
@@ -1,4 +1,4 @@
-<?php include('../includes/header.php'); ?>////////////////////////////////////////
+<?php include('../includes/header.php'); ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -18,25 +18,25 @@
     <div class="formgroup">
         <div>
             <input type="text" id="prenom" name="prenom" placeholder="Prénom" required>
-            <span id="error-prenom" class="error-message" style="color: red; font-size: 13px; display: none; text-align: left; margin-top: -5px; margin-bottom: 5px;"></span>
+            <span id="error-prenom" class="error-message"></span>
         </div>
         <div>
             <input type="text" id="nom" name="nom" placeholder="Nom" required>
-            <span id="error-nom" class="error-message" style="color: red; font-size: 13px; display: none; text-align: left; margin-top: -5px; margin-bottom: 5px;"></span>
+            <span id="error-nom" class="error-message"></span>
         </div>
     </div>
 
     <input type="email" id="email" name="email" placeholder="Email" required>
-    <span id="error-email" class="error-message" style="color: red; font-size: 13px; display: none; text-align: left; margin-top: -5px; margin-bottom: 5px;"></span>
+    <span id="error-email" class="error-message"></span>
 
     <input type="tel" id="telephone" name="telephone" placeholder="Numéro de téléphone" required>
-    <span id="error-telephone" class="error-message" style="color: red; font-size: 13px; display: none; text-align: left; margin-top: -5px; margin-bottom: 5px;"></span>
+    <span id="error-telephone" class="error-message"></span>
 
     <div style="position: relative;">
         <input type="password" id="password" name="password" placeholder="Mot de passe" maxlength="32" required>
         <span id="toggle-password" style="position: absolute; right: 15px; top: 12px; cursor: pointer; z-index: 10;">👁️</span>
     </div>
-    <div style="display: flex; justify-content: space-between; margin-top: -5px; margin-bottom: 5px;">
+    <div>
         <span id="error-password" class="error-message" style="color: red; font-size: 13px; display: none;"></span>
         <span id="password-counter" class="char-counter" style="font-size: 12px; color: #888;">32 caractères restants</span>
     </div>
@@ -46,16 +46,16 @@
     
     <h3>Adresse de livraison</h3>
     <input type="text" id="adresse" name="adresse" placeholder="Adresse complète (N°, rue...)" required>
-    <span id="error-adresse" class="error-message" style="color: red; font-size: 13px; display: none; text-align: left; margin-top: -5px; margin-bottom: 5px;"></span>
+    <span id="error-adresse" class="error-message"></span>
 
     <div class="formgroup">
         <div>
             <input type="text" id="code_postal" name="code_postal" placeholder="Code Postal" required>
-            <span id="error-code_postal" class="error-message" style="color: red; font-size: 13px; display: none; text-align: left; margin-top: -5px; margin-bottom: 5px;"></span>
+            <span id="error-code_postal" class="error-message"></span>
         </div>
         <div>
             <input type="text" id="ville" name="ville" placeholder="Ville" required>
-            <span id="error-ville" class="error-message" style="color: red; font-size: 13px; display: none; text-align: left; margin-top: -5px; margin-bottom: 5px;"></span>
+            <span id="error-ville" class="error-message"></span>
         </div>
     </div>
 
